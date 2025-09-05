575d307e43b9053fd5270bf93410618251803cf7
Luboš Luňák <l.lunak@centrum.cz>
2025-09-05 10:43:29 +0200

    fix undertaking for crematorium and DBH burn pit
    This is actually fairly complicated. Crematorium (and DBH burn pit)
    can have two kinds of work, similarly to e.g. campfire, those being cremating
    and burning other things (apparel, etc.). Cremating should be undertaking
    (so that its setting actually apply and e.g. prevent children from doing
    the work), but burning cannot be, because RecipeDef of BurnApparel etc. has
    requiredGiverWorkType requiring hauling. Therefore there need to be actually
    two WorkGiverDef's, one for each work type, just like there is
    DoBillsCookCampfire and DoBillsHaulCampfire for campfire.
    
    One more complication is that both those WorkGiverDef's need to differ in verb
    and gerund fields, otherwise the right-mouse popup apparently somehow ignores
    one of them if it's the same text, which would prevent cremating and only
    burning could be prioritized.

---
+++ Source/Assemblies/PatchOps/PatchOperationDuplicate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Verse;
+
+// Duplicates the given xpath (should be renamed or similar afterwards).
+namespace ManyJobs.PatchOps
+{
+    public class PatchOperationDuplicate : PatchOperationPathed
+    {
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            bool result = false;
+            foreach (object item in xml.SelectNodes(xpath))
+            {
+                XmlNode xmlNode = item as XmlNode;
+                XmlNode parentNode = xmlNode.ParentNode;
+                if( parentNode != null )
+                {
+                    result = true;
+                    parentNode.AppendChild(xmlNode.OwnerDocument.ImportNode(xmlNode, deep: true));
+                }
+            }
+            return result;
+        }
+    }
+}
